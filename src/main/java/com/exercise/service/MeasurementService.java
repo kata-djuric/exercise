ae028a90a7c58e3436f27b50d4fdec490975cef3
katarina.djuric <katarinadjuric30@gmail.com>
2020-12-29 21:38:25 +0100

    Javadoc added for functions for setting and extracting data per sensor

--- src/main/java/com/exercise/service/MeasurementService.java
+++ src/main/java/com/exercise/service/MeasurementService.java
@@ -25,6 +25,12 @@ public class MeasurementService {
     @Autowired
     private DSLContext create;
 
+    /**
+     * Writing into table MEASUREMENT data received from sensor
+     *
+     * @param measurementPerSensor - data received from sensor
+     */
+
     @Transactional
     public void addMeasurementPerSensor(MeasurementPerSensor measurementPerSensor) {
         MeasurementRecord measurementRecord = create.newRecord(MEASUREMENT);
@@ -33,6 +39,16 @@ public class MeasurementService {
         measurementRecord.setTimestamp(measurementPerSensor.getTimestamp());
         measurementRecord.store();
     }
+
+    /**
+     * For the given sensor id and time range, extracts from the table MEDIAN_NOISE_LEVEL_PER_SENSOR
+     * medial noise for that sensor and the timestamp when it is calculated.
+     *
+     * @param sensorId  - sensor id
+     * @param startTime - start time for extracting medial noise per sensor
+     * @param endTime   - end time for extracting medial noise per sensor
+     * @return list of values and timestamps
+     */
 
     @Transactional
     public List<MedianNoisePerSensorResponse> getMedialNoiseMeasurementInTimeRange(
@@ -47,21 +63,32 @@ public class MeasurementService {
                 .fetch().into(MedianNoisePerSensorResponse.class);
     }
 
+    /**
+     * Calculates medial noise per sensor for the previous hour and writes it into table MEDIAN_NOISE_LEVEL_PER_SENSOR.
+     */
+
     public void setMedialNoisePerSensor() {
         LocalDateTime currentLocalDateTime = LocalDateTime.now();
         Timestamp currentTimestamp = Timestamp.valueOf(currentLocalDateTime);
         Timestamp currentTimestampSubHour = Timestamp.valueOf(currentLocalDateTime.minusHours(1));
 
         Map<Long, BigDecimal> record = getMedialNoiseMeasurementPerSensor(currentTimestampSubHour, currentTimestamp);
-        record.keySet().forEach(key -> {
-            create.insertInto(MEDIAN_NOISE_LEVEL_PER_SENSOR,
-                    MEDIAN_NOISE_LEVEL_PER_SENSOR.SENSOR_ID,
-                    MEDIAN_NOISE_LEVEL_PER_SENSOR.VALUE,
-                    MEDIAN_NOISE_LEVEL_PER_SENSOR.TIMESTAMP)
-                    .values(key, record.get(key), Timestamp.valueOf(currentTimestamp.toString()))
-                    .execute();
-        });
+        record.keySet().forEach(key -> create.insertInto(MEDIAN_NOISE_LEVEL_PER_SENSOR,
+                MEDIAN_NOISE_LEVEL_PER_SENSOR.SENSOR_ID,
+                MEDIAN_NOISE_LEVEL_PER_SENSOR.VALUE,
+                MEDIAN_NOISE_LEVEL_PER_SENSOR.TIMESTAMP)
+                .values(key, record.get(key), Timestamp.valueOf(currentTimestamp.toString()))
+                .execute());
     }
+
+    /**
+     * From existing table MEASUREMENT, for each sensor medial noise is calculated for the given
+     * time range.
+     *
+     * @param currentTimestampSubHour - start of the time range
+     * @param currentTimestamp        - end of the time range
+     * @return map where the key is sensor id and value medial value
+     */
 
     private Map<Long, BigDecimal> getMedialNoiseMeasurementPerSensor(Timestamp currentTimestampSubHour,
                                                                      Timestamp currentTimestamp) {
